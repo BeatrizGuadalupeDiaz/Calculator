eae76f2bdfeaa9ceb97f97031032152f18728219
BeatrizGuadalupeDiaz <beatrizdiaz13y19@gmail.com>
2023-10-13 22:59:06 -0600

    Add new project to GitHub-> Calculator

--- App.tsx
+++ App.tsx
@@ -1,11 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+const SET_VALUE = 1;
+const MINIMUM_VALUE = 0;
 
 export default function App() {
+  
+  const [counter, setCounter] = React.useState(0);
+
+  //functions
+  const plus = () => {
+    setCounter(counter + SET_VALUE);
+  };
+  const minus = () => {
+    if (counter <= MINIMUM_VALUE) {
+      return;
+    }
+    setCounter(counter - SET_VALUE);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Calculator</Text>
+      <View>
+        <TouchableOpacity style={styles.btn} onPress={plus}>
+          <Text style={styles.text}>+</Text>
+        </TouchableOpacity>
+        <Text style={styles.text}>{counter}</Text>
+        <TouchableOpacity style={styles.btn} onPress={minus}>
+          <Text style={styles.text}>-</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +38,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  btn: {
+    width: 40,
+    height: 30,
+    backgroundColor: "lightblue",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 20,
+    textAlign: "center",
+    fontWeight: "600",
   },
 });
